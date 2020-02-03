3448819a834367cd3d14357e7ce5173274735965
G9us <arturst95@gmail.com>
2020-02-03 13:58:48 +0000

    Initial commit

---
+++ Source/ThiefSimulator/ThiefSimulatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThiefSimulatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIEFSIMULATOR_API AThiefSimulatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
